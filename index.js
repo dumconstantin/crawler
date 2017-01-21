ed430f12227efe994b4e2c4c91aae7b86c139415
dumconstantin <dum.constantin@gmail.com>
2017-01-21 10:35:53 +0200

    Add jsdom and bluebird deps. Add html handling in stream.

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
+'use strict'
 const most = require('most')
 const request = require('request')
 const EventEmitter = require('events')
 const emitter = new EventEmitter()
+const jsdom = require('jsdom')
+const Promise = require('bluebird')
 
 const startUrl = 'https://google.com'
 
-const stream = most.fromEvent('data', emitter)
+const stream = most
+  .fromEvent('html', emitter)
+  .chain(x => {
+    return most.fromPromise(new Promise((resolve, reject) => {
+      jsdom.env('<p>asd</p>', ['http://code.jquery.com/jquery.js'], (err, window) => {
+        if (err) {
+          reject(err)
+        } else {
+          let links = window.document.querySelector('a').innerText
+          resolve(links)
+        }
+      })
+    }))
+  })
 
 stream
-  .observe(x => {
-    console.log(x)
+  .subscribe({
+    next: x => console.log('Data', x),
+    complete: x => console.log('Finished', x),
+    error: x => console.error('Error', x)
   })
 
 request(startUrl, (err, response, body) => {
-  emitter.emit('data', response)
+  emitter.emit('html', body)
 })
